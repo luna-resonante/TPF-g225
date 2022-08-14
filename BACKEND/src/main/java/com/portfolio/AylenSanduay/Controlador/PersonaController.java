7b5f7379d174c31479d31928e3061b6178c83323
luna-resonante <lunaresonante2021@hotmail.com>
2022-08-14 12:33:35 -0300

    model service y backend

--- BACKEND/src/main/java/com/portfolio/AylenSanduay/Controlador/PersonaController.java
+++ BACKEND/src/main/java/com/portfolio/AylenSanduay/Controlador/PersonaController.java
@@ -5,6 +5,7 @@ import com.portfolio.AylenSanduay.Entidad.Persona;
 import com.portfolio.AylenSanduay.Interfaz.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,19 +25,21 @@ public class PersonaController {
     public List <Persona> getPersona(){
      return ipersonaService.getPersona();
 }
-    
+    @PreAuthorize ("hasRole('ADMIN')")
     @PostMapping("/persona/crear")
     public String createPersona(@RequestBody Persona persona){
      ipersonaService.savePersona(persona);
      return "La persona fue creada correctamente";
     }
     
+    @PreAuthorize ("hasRole('ADMIN')")
     @DeleteMapping("/persona/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
     ipersonaService.deletePersona(id);
     return "La persona fue eliminada correctamente";
     }
     
+    @PreAuthorize ("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,               
                                @RequestParam("nombre") String nuevoNombre, 
---
+++ BACKEND/src/main/java/com/portfolio/AylenSanduay/Security/Service/UsuarioService.java
@@ -0,0 +1,32 @@
+package com.portfolio.AylenSanduay.Security.Service;
+
+import com.portfolio.AylenSanduay.Security.Entity.Usuario;
+import com.portfolio.AylenSanduay.Security.Repository.iUsuarioRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class UsuarioService {
+
+    @Autowired
+    iUsuarioRepository iusuarioRepository;
+
+    public Optional<Usuario> geyByNombreUsuario(String nombreUsuario) {
+        return iusuarioRepository.findByNombreUsuario(nombreUsuario);
+    }
+
+    public boolean existsByNombreUsuario(String nombreUsuario) {
+        return iusuarioRepository.existsByNombreUsuario(nombreUsuario);
+    }
+
+    public boolean existsByEmail(String email) {
+        return iusuarioRepository.existsByEmail(email);
+    }
+    
+    public void save (Usuario usuario){
+        iusuarioRepository.save(usuario);
+    }
+}
