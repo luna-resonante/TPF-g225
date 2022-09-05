961a9f16c41aca5752b20455827ea19c4c08cb26
luna-resonante <lunaresonante2021@hotmail.com>
2022-09-05 14:23:54 -0300

    a

--- FRONTEND/src/app/app.module.ts
+++ FRONTEND/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './componentes/hys/hys.component';
 import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 import { FooterComponent } from './componentes/footer/footer.component';
-import { HttpClient , HttpClientModule} from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { interceptorProvider } from './services/interceptor-service';
--- FRONTEND/src/app/services/persona.service.ts
+++ FRONTEND/src/app/services/persona.service.ts
@@ -7,7 +7,7 @@ import { persona } from '../models/persona.model';
   providedIn: 'root'
 })
 export class PersonaService {
-URL= 'Http://localhost8080/personas/';
+URL= 'http://localhost:8080/personas/';
 
   constructor(private http: HttpClient ) { }
 
